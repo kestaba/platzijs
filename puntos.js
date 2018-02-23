03d927e8dc75cdf8914e006b0acc9d59786d48e2
kestaba <kestaba.dev@gmail.com>
2018-02-23 02:41:27 -0400

    Prototipos en js.

--- puntos.js
+++ puntos.js
@@ -1,22 +1,29 @@
-const p1 = {
-    x: 0,
-    y: 4,
-    moverEnX: function (x){ this.x += x },
-    moverEnY: function (y){ this.y += y }
+function Punto(x, y){
+    this.x = x
+    this.y = y
 }
 
-const p2 = {
-    x:3,
-    y: 0,
-    moverEnX: function (x){ this.x += x },
-    moverEnY: function (y){ this.y += y }
+Punto.prototype.moverEnX = function moverEnX(x){ 
+    this.x += x 
 }
 
-function distancia(p1, p2){
-    const x = p1.x - p2.x
-    const y = p1.y - p2.y
+Punto.prototype.moverEnY = function moverEnY(y){
+    this.y += y 
+}
+
+Punto.prototype.distancia = function distancia(p){
+    const x = this.x - p2.x
+    const y = this.y - p2.y
 
     return Math.sqrt(x * x + y * y)
 }
 
-console.log(distancia(p1, p2))
+const p1 = new Punto(0, 4)
+const p2 = new Punto(3, 0)
+
+console.log(p1.distancia(p2))
+console.log(p2.distancia(p1))
+p1.moverEnX(10)
+console.log(p1.distancia(p2))
+p2.moverEnY(-4)
+console.log(p1.distancia(p2))
