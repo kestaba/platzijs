6a8f8a89e13a88813afd661c9d0f232d13aa7a0d
kestaba <kestaba.dev@gmail.com>
2018-02-23 02:53:54 -0400

    Object.create en js.

--- puntos.js
+++ puntos.js
@@ -1,25 +1,31 @@
-function Punto(x, y){
-    this.x = x
-    this.y = y
+const Punto = {
+    init: function init(x, y){
+        this.x = x
+        this.y = y
+    },
+
+    moverEnX: function moverEnX(x){
+        this.x += x
+    },
+
+    moverEnY: function moverEnY(y){
+        this.y += y
+    },
+
+    distancia: function distancia(p){
+        const x = this.x - p2.x
+        const y = this.y - p2.y
+
+        return Math.sqrt(x * x + y * y)
+    }
 }
 
-Punto.prototype.moverEnX = function moverEnX(x){ 
-    this.x += x 
-}
 
-Punto.prototype.moverEnY = function moverEnY(y){
-    this.y += y 
-}
+const p1 = Object.create(Punto)
+const p2 = Object.create(Punto)
 
-Punto.prototype.distancia = function distancia(p){
-    const x = this.x - p2.x
-    const y = this.y - p2.y
-
-    return Math.sqrt(x * x + y * y)
-}
-
-const p1 = new Punto(0, 4)
-const p2 = new Punto(3, 0)
+p1.init(0, 4)
+p2.init(3, 0)
 
 console.log(p1.distancia(p2))
 console.log(p2.distancia(p1))
