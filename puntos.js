6b7cb737d0bdeea45af31d9474e8a7a59a8f6a62
kestaba <kestaba.dev@gmail.com>
2018-02-23 03:00:28 -0400

    Class en js.

--- puntos.js
+++ puntos.js
@@ -1,18 +1,19 @@
-const Punto = {
-    init: function init(x, y){
+class Punto {
+
+    constructor(x, y){
         this.x = x
         this.y = y
-    },
+    }
 
-    moverEnX: function moverEnX(x){
+    moverEnX(x){
         this.x += x
-    },
+    }
 
-    moverEnY: function moverEnY(y){
+    moverEnY(y){
         this.y += y
-    },
+    }
 
-    distancia: function distancia(p){
+    distancia(p){
         const x = this.x - p2.x
         const y = this.y - p2.y
 
@@ -20,12 +21,8 @@ const Punto = {
     }
 }
 
-
-const p1 = Object.create(Punto)
-const p2 = Object.create(Punto)
-
-p1.init(0, 4)
-p2.init(3, 0)
+const p1 = new Punto(0, 4)
+const p2 = new Punto(3, 0)
 
 console.log(p1.distancia(p2))
 console.log(p2.distancia(p1))
